a379847ad7ee57e73115688c58e90e883951f039
bdb2381 <brad.beggs@gmail.com>
2020-12-14 20:09:21 -0800

    find numbers smaller than other numbers. solution 2

--- arrays/numbersSmallerThanCurrentNumber.js
+++ arrays/numbersSmallerThanCurrentNumber.js
@@ -7,7 +7,7 @@
 // 2 <= nums.length <= 500
 // 0 <= nums[i] <= 100
 
-//Version 1
+// Version 1
 // time O(n^2) space O(n)
 var smallerNumbersThanCurrent = function(nums) {
   let map = {}
@@ -24,5 +24,21 @@ var smallerNumbersThanCurrent = function(nums) {
     output.push(counter)
   }
   
+  return(output)
+};
+
+// Version 2
+// time O(n^2) space O(n)
+var smallerNumbersThanCurrent = function(nums) {
+  let output = []
+  
+  for (number in nums){
+      let count = 0
+
+      for(n in nums){
+          if(nums[n] < nums[number]) count++
+      }
+      output.push(count)
+  }   
   return(output)
 };
