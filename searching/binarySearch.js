a962f75480040ab75d19fc2d7d98683f7ac223fc
bdb2381 <brad.beggs@gmail.com>
2020-12-22 18:43:44 -0800

    binary search. Solution 1

---
+++ searching/binarySearch.js
@@ -0,0 +1,43 @@
+// via AlgoExpert
+
+// Solution 1
+// O(n**2) run time
+const arraya = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]
+const target = 33
+
+function binarySearch(array, target) {
+	let result = search(array, target, 0, array.length-1)
+	return result
+}
+
+function search(array, target, left, right) {
+	if (left > right){   // if left pointer exceeds right, then no match is found
+		return -1
+	}
+	let half = Math.floor((left + right)/2) 
+	// index half is test case as the final remaining number is also the half. 
+	
+	let possibleMatch = array[half]
+	// get value in the half index
+	
+	if (target === possibleMatch){ 
+	// when we have a match, return the idex number 
+		return half
+	}
+	
+	else if (target < array[half]){
+	// check left side, if the target number is less then the number in array[half]
+		return search(array, target, 0, half-1)  
+	// recursive call resetting the right pointer to half-1  since we checked half already
+	// this allows us to check on the left side [0 thru half-1]
+	}	
+	
+	else if (target > array[half]){
+	// check right side, if the target number is less then the number in array[half]
+		return search(array, target, half+1, right)
+	// recursive call resetting the left pointer to half +1 since we checked half already
+	// this allows us to check the right size [half + 1 thur array.length]	
+	}
+}
+
+
