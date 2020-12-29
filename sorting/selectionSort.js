3cb1a19c9eca80dfe9b30cdd0bb27fe38c93792c
bdb2381 <brad.beggs@gmail.com>
2020-12-28 21:56:41 -0800

    selection sort. half attempt from memory

--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -30,3 +30,35 @@ function swap(i, j, array){
 
 // Do not edit the line below.
 // exports.selectionSort = selectionSort;
+
+
+
+// Attempt 3 with explantion. Almost from memory
+function selectionSort(array) {
+	// declare startingIndexUnsorted, which is starting index of unsorted array
+	// need two loops, one that runs everything and the swap and an inner that tracks only the smallest ind
+	
+	let startingIndexUnsorted = 0
+	
+	while (startingIndexUnsorted < array.length - 1){  // keep going until we check everything
+		let smallestIndex = startingIndexUnsorted  // initialize to first position in case this position holds smallest number
+
+		for (let i = startingIndexUnsorted + 1; i < array.length; i++){  // find smallest in the unsorted array
+			if(array[smallestIndex] > array[i]){  // if number in smallestIndex pointer is bigger then, 
+				 smallestIndex = i									// track which index has smallest 
+
+			}
+		}
+			swap(startingIndexUnsorted, smallestIndex, array) // swap the numbers in the smallest and the former startingIndexUnosrted
+			startingIndexUnsorted++
+	}
+  return array	
+	
+}
+
+function swap(i, j, array){
+	[array[i], array[j]] = [array[j], array[i]]
+}
+
+// Do not edit the line below.
+exports.selectionSort = selectionSort;
