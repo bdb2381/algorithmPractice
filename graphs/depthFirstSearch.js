9703fd38e9206250cbd03a06fff7e2409b8e7fc3
bdb2381 <brad.beggs@gmail.com>
2021-01-04 17:33:49 -0800

    depthFirstSearch

---
+++ graphs/depthFirstSearch.js
@@ -0,0 +1,37 @@
+// Do not edit the class below except
+// for the depthFirstSearch method.
+// Feel free to add new properties
+// and methods to the class.
+// from AlgoExpert
+
+
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  // Time  is O(v + e)
+  // Space is O(v)
+  // v is each vertices (aka node)
+  // e is an edge a parent has to its child(ren) 
+  depthFirstSearch(array) {
+		// add name of node eah t ime depthFirstSearch function is called
+		array.push(this.name)  
+		
+		// for every child in hte parent array, call the DFS function on it
+		// pass the child.DFS the array so to give access to append the name
+		for (let child of this.children){	
+			child.depthFirstSearch(array)
+		}
+		return array
+  }
+}
+
+// Do not edit the line below.
+exports.Node = Node;
