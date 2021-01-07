0adf3fd8acae334607c601e26c52be6b3f673ff7
bdb2381 <brad.beggs@gmail.com>
2021-01-07 14:22:33 -0800

    two number sum practice with two loops.

--- arrays/twoSum.js
+++ arrays/twoSum.js
@@ -24,3 +24,28 @@ var twoSum = function(nums, target) {
 
     }    
 };
+
+
+// Solution 2
+// Time O(n**2) 
+// Space O(n) since no new space is created 
+let array = [3, 5, -4, 8, 11, 1, -1, 6]
+let targetSum = 10
+function twoNumberSum(array, targetSum) {
+	let results = []
+	
+	for (let i = 0; i < array.length; i++){
+		for(let y = i+1; y < array.length; y++){
+			let sum = array[i] + array[y]
+			
+			if (sum === targetSum){
+				results.push(array[i], array[y])
+			}
+		}
+	}
+	
+	return results
+}
+
+// Do not edit the line below.
+exports.twoNumberSum = twoNumberSum;
