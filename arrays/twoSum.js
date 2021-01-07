606fed5069272f94da6023a8af22bf283715dfee
bdb2381 <brad.beggs@gmail.com>
2021-01-07 15:40:01 -0800

    two number solution 4 in O(n log(n)) time

--- arrays/twoSum.js
+++ arrays/twoSum.js
@@ -47,5 +47,59 @@ function twoNumberSum(array, targetSum) {
 	return results
 }
 
-// Do not edit the line below.
-exports.twoNumberSum = twoNumberSum;
+// Solution 3
+// Time O(n) since going through array once
+// Space O(n) since memory size increases with input due to object
+function twoNumberSum(array, targetSum) {
+	let dictionary = {}
+
+	for (let currentNumber of array){
+		let sum = targetSum - currentNumber
+		console.log(sum, dictionary)
+		if (sum in dictionary){
+			return[sum, currentNumber]
+		} else {
+			dictionary[currentNumber] = true
+			}	
+		}
+return []
+}
+
+
+// Solution 4
+// Time O(n log(n)) because sort() runs inn this time
+// Space O(1) because no new space is created 
+// this solution has better space use but is slightly slower then solution 2
+function twoNumberSum(array, targetSum) {
+
+	array.sort((a,b,) =>  a - b)  // sort() is O(nlog(n) time)
+	let left = 0 									// initialize pointer
+	let right = array.length - 1	// initialize pointer
+	
+	while (left < right){					// when left pointer is bigger, we've searched entire array
+		let sum = array[left] + array[right] 
+		if (sum === targetSum){			
+			return [array[left], array[right]]
+		}
+		else if (sum < targetSum){	
+			// because array is sorted, and sum is less then target,
+			// we know we need to increase a number to maket sum closer to target
+			// thus, change left pointer position by 1 to check the next element
+			left++	
+		}
+		else if (sum > targetSum){
+			// because array is sorted, and sum is greater then target,
+			// we know we need to decrease a number to maket sum closer to target
+			// thus, change right pointer (which are larger numbers) position by 1 
+			// to check the next element
+			right--
+		}
+	}
+	
+	return []
+
+}
+
+
+
+
