cf45cd55c6a9936882477ab2a9f9191244d134bf
bdb2381 <brad.beggs@gmail.com>
2021-02-23 14:08:22 -0800

    Node Depth Attempt 2

--- BinarySearchTrees & BinarySarch/nodeDepth.js
+++ BinarySearchTrees & BinarySarch/nodeDepth.js
@@ -1,3 +1,10 @@
+
+// from AlgoExpert
+
+
+// Attempt 1
+// Time: O(n**) since using a reducer
+// Space: O(h) since tree height is what is on call stack at any one time, worst case
 function nodeDepths(root) {
 	let runningDepth = -1 
 	let arrayToSum = []
@@ -42,6 +49,28 @@ class BinaryTree {
     this.right = null;
   }
 		
+}
+
+// Do not edit the line below.
+exports.nodeDepths = nodeDepths;
+
+
+
+// Attempt 2
+// Time: O(n) since need to touch each
+// Space: O(h) since call stack worse case holds depth of tree
+function nodeDepths(root, depth = 0) {
+  if (root === null) return 0
+	return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1)
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 // Do not edit the line below.
