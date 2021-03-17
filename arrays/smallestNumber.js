b7ad86955899b34fb3129ed19ce49b030a75b657
bdb2381 <brad.beggs@gmail.com>
2021-03-17 11:12:19 -0700

    Smallest Number. Second attempt. 23 minutes

--- arrays/smallestNumber.js
+++ arrays/smallestNumber.js
@@ -49,5 +49,66 @@ return smallestPair
 exports.smallestDifference = smallestDifference;
 
 
-// -1, 3 5 10 20 28
-// 15 17 26 134 135
+/////////////////////////
+//Second Attempt - 23:30 minutes
+// wrote algo first and then leetCode
+
+//sort both arrays in asc order
+//asesign pointers for each
+	// init currentSmallest & smallest to infinity
+//create results array
+//while loop through both with && sequence 
+	// in loop, assign each idx to human readable stable var
+// on each loop, compare each idx to the other
+	// if firstNumber is < secondNumber
+		// subtract secondNumber - firstNumber and assign to currentSmalleste
+		// increase firstNumber idx++  because firstNumber is potential too far from secondnumber
+	// else if secondNumber < firstNumber
+		// subtract firstNumber - secondNumber and assign to currentSmallest
+		// increase secondNumber idx++ to move secondNumber closer to first number
+	// else numbers are === and return the result []
+	
+	//handle comparision of differences
+	// if smallestDifference > currentDifference
+		// smallestDifference = currentDifference 
+		// assign firstNumber and secondNumber to results array
+	
+	// outside of loop, when complete return results array
+
+function smallestDifference(arrayOne, arrayTwo) {
+	arrayOne.sort((a, b) => a-b)
+	arrayTwo.sort((a, b) => a-b)
+	
+	let oneIdx = 0, twoIdx = 0 
+	let currentSmallest = Infinity 
+	let smallest = Infinity
+	let result = [] 
+
+	while(oneIdx < arrayOne.length && twoIdx < arrayTwo.length){
+		let firstNumber = arrayOne[oneIdx]
+		let secondNumber = arrayTwo[twoIdx]
+		
+		if (firstNumber < secondNumber){
+			currentSmallest = secondNumber - firstNumber
+			oneIdx++
+		}
+		else if (secondNumber < firstNumber) {
+			currentSmallest = firstNumber - secondNumber
+			twoIdx++
+		}
+		else {
+			return result=[firstNumber, secondNumber]
+		}
+		
+		if (smallest > currentSmallest){
+			smallest = currentSmallest
+			result=[firstNumber, secondNumber]
+		}
+		
+	}
+
+	return result
+}
+
+// Do not edit the line below.
+exports.smallestDifference = smallestDifference;
